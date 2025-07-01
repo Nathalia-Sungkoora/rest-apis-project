f57457fd51ac4c0c4070ad6c838db30149cc9283
Nathalia Sungkoora <nathalia.sungkoora@sambeconsulting.com>
2025-07-01 22:14:41 +0200

    Adding changes to fix errors.

--- schemas.py
+++ schemas.py
@@ -52,4 +52,4 @@ class UserSchema(Schema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
     password = fields.Str(required=True, load_only=True)
-    email = fields.Email(required=True) #added email field
+    
