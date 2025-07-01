1897c745ff2007e4ecee7b1c6e7e6e05af51d3e9
Nathalia Sungkoora <nathalia.sungkoora@sambeconsulting.com>
2025-07-01 21:44:59 +0200

    Reverting back to changes made in working render commit.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,20 +1,5 @@
-# #!/bin/sh
-
-# flask db upgrade
-
-# exec gunicorn --bind 0.0.0.0:80 "app:create_app()"
-
-
-# #!/bin/sh
-
-# flask db upgrade
-
-# exec gunicorn --bind 0.0.0.0:80 --factory app:create_app
-
-
 #!/bin/sh
 
 flask db upgrade
 
-exec gunicorn --bind 0.0.0.0:80 wsgi:app
-
+exec gunicorn --bind 0.0.0.0:80 "app:create_app()"
