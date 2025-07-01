7f7c26cd0bb4c08e7e930473f6b9222de372437d
Nathalia Sungkoora <nathalia.sungkoora@sambeconsulting.com>
2025-07-01 13:58:00 +0200

    add model changes and migrations, make sure migrations run before app starts.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask, jsonify
 from flask import Flask
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 from blocklist import BLOCKLIST
 
 from db import db
@@ -16,6 +18,7 @@ from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
